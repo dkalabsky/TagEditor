45a5195f733f920f6852bd3e93583b7d9cccfd5f
dkalabsky <dkalabsky@gmail.com>
2023-05-22 03:05:21 +0300

    fix for work

--- api/src/main/java/home/dev/resource/TagEditorApi.java
+++ api/src/main/java/home/dev/resource/TagEditorApi.java
@@ -9,7 +9,7 @@ import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 
-@Api(value = "/tag-editor/")
+@Api(value = "/tag-editor")
 @RequestMapping("/tag-editor")
 public interface TagEditorApi {
 
